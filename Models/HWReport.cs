bdc786c769ae0366d812a6fe8ec6fe9c196129cf
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-11 16:44:10 +0300

    Добавлено поле для типа ОЗУ

--- Models/HWReport.cs
+++ Models/HWReport.cs
@@ -8,12 +8,11 @@
 
         public double RAMSize { get; set; }                                     // Общее количество ОЗУ
         public int RAMSpeed { get; set; }                                       // Частота ОЗУ
+        public string RAMType { get; set; } = "Unknown";                        // Тип ОЗУ
 
         public List<string> VideoCards { get; } = [];                           // Названия видеокарт
 
         public List<string> ModelsDrives { get; } = [];                         // Модели накопителей
         public List<double> DrivesSize { get; } = [];                           // Объемы накопителей
-
-        
     }
 }

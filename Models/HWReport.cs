a22852d560fb59200979aa39b4b7c878d0793ffd
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-11 20:02:00 +0300

    Добавлена поля для хранения видеопамяти, данных о материнской плате и BIOS

--- Models/HWReport.cs
+++ Models/HWReport.cs
@@ -6,14 +6,23 @@
         public int CoresCount { get; set; }                                     // Количество ядер процессора
         public int CurrentClockSpeed { get; set; }                              // Базовая частота процессора   
 
-        public double RAMSize { get; set; }                                     // Общее количество ОЗУ
-        public int RAMSpeed { get; set; }                                       // Частота ОЗУ
         public string RAMType { get; set; } = "Unknown";                        // Тип ОЗУ
+        public double RAMSize { get; set; }                                     // Объем ОЗУ
+        public int RAMSpeed { get; set; }                                       // Частота ОЗУ
 
-        public List<string> VideoCards { get; } = [];                           // Названия видеокарт
-        public double AdapterRAM { get; set; }                                  // Объем видеопамяти
+        public List<string> VideoCards { get; } = [];                           // Название видеокарты
+        public List<double> AdaptersRAM { get; } = [];                          // Объем видеопамяти
 
-        public List<string> ModelsDrives { get; } = [];                         // Модели накопителей
-        public List<double> DrivesSize { get; } = [];                           // Объемы накопителей
+        public List<string> ModelsDrives { get; } = [];                         // Модель накопителя
+        public List<double> DrivesSize { get; } = [];                           // Объем накопителя
+
+        public string BaseBoardModel { get; set; } = "Unknown";                 // Модель основной платы
+        public string BaseBoardManufacturer { get; set; } = "Unknown";          // Производитель основной платы
+        public string BaseBoardVersion { get; set; } = "Unknown";               // Версия основной платы
+        public string BaseBoardStatus { get; set; } = "Unknown";                // Статус состояния основной платы
+
+        public string BIOSVersion { get; set; } = "Unknown";                    // Версия BIOS
+        public DateTime? BIOSReleaseDate { get; set; }                          // Дата релиза BIOS      
+        public string BIOSManufacturer { get; set; } = "Unknown";               // Производитель BIOS
     }
 }

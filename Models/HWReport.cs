e76196adb60237396db488c7707f0cd5de5eb65e
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-11 16:33:53 +0300

    Информация о накопителях разделена на 2 переменные - модель и объем

--- Models/HWReport.cs
+++ Models/HWReport.cs
@@ -5,9 +5,15 @@
         public string ProcessorName { get; set; } = "Unknown";                  // Название процессора
         public int CoresCount { get; set; }                                     // Количество ядер процессора
         public int CurrentClockSpeed { get; set; }                              // Базовая частота процессора   
+
         public double RAMSize { get; set; }                                     // Общее количество ОЗУ
         public int RAMSpeed { get; set; }                                       // Частота ОЗУ
-        public List<string> VideoCards { get; } = new List<string>();           // Названия видеокарт
-        public List<string> Drives { get; } = new List<string>();               // Накопители (название, объем)
+
+        public List<string> VideoCards { get; } = [];                           // Названия видеокарт
+
+        public List<string> ModelsDrives { get; } = [];                         // Модели накопителей
+        public List<double> DrivesSize { get; } = [];                           // Объемы накопителей
+
+        
     }
 }

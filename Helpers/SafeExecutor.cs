04852417dcd4bc053b1da169011819e6eb3531ae
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-13 18:05:02 +0300

    Добавлена обработка ошибок прав доступа

--- Helpers/SafeExecutor.cs
+++ Helpers/SafeExecutor.cs
@@ -26,15 +26,25 @@ namespace Diagnostish.Helpers
                 }
                 wmiAction(collection);
             }
+            catch (UnauthorizedAccessException)
+            {
+                criticalErrors.Add($"Нет прав доступа для получения данных о/об {context}. Запустите утилиту от имени администратора!");
+                Log.Error($"Отказано в доступе к WMI при запросе `{query}` для {context}.");
+            }
+            catch (ManagementException mex) when (mex.ErrorCode == ManagementStatus.AccessDenied)
+            {
+                criticalErrors.Add($"Доступ к WMI для {context} запрещён. Проверьте права администратора.");
+                Log.Error($"WMI AccessDenied при запросе `{query}`.");
+            }
             catch (ManagementException mex) when (mex.ErrorCode == ManagementStatus.Timedout)
             {
                 criticalErrors.Add($"Получение данных о/об {context} остановлено по таймауту (WMI завис).");
-                Log.Fatal(mex, $"Критический таймаут WMI при запросе '{query}' для {context}");
+                Log.Fatal(mex, $"Критический таймаут WMI при запросе `{query}` для {context}");
             }
             catch (Exception ex)
             {
                 errors.Add($"Ошибка получения данных о/об {context}: {ex.Message}");
-                Log.Error(ex, $"Сбой выполнения WMI запроса '{query}'");
+                Log.Error(ex, $"Сбой выполнения WMI запроса `{query}`");
             }
         }
     }

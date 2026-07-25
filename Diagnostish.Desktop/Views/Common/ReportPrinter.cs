4ea9e28d849ee426c0cf1e7947162f69cbc2950b
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-25 05:28:37 +0300

    Базовый класс для принтеров отчетов, содержащий базовый вывод ошибок (наследует контракт вывода)

---
+++ Diagnostish.Desktop/Views/Common/ReportPrinter.cs
@@ -0,0 +1,28 @@
+﻿using Diagnostish.Domain.Interfaces;
+
+namespace Diagnostish.Desktop.Views.Common;
+
+public abstract class ReportPrinter<TReport> : IReportPrinter<TReport>
+{
+    public void Print(TReport report)
+    {
+        PrintReport(report);
+    }
+
+    protected abstract void PrintReport(TReport report);
+
+    protected void PrintIssues(List<string> errors, List<string> criticalErrors)
+    {
+        if (errors.Count > 0)
+        {
+            ColorPrinter.WriteLineColored("\n* ПРЕДУПРЕЖДЕНИЯ *", ConsoleColor.DarkYellow);
+            foreach (var warn in errors) ColorPrinter.WriteLineColored($"    - {warn}", ConsoleColor.Yellow);
+        }
+
+        if (criticalErrors.Count > 0)
+        {
+            ColorPrinter.WriteLineColored("\n* КРИТИЧЕСКИЕ ОШИБКИ *", ConsoleColor.DarkRed);
+            foreach (var warn in criticalErrors) ColorPrinter.WriteLineColored($"    - {warn}", ConsoleColor.Red);
+        }
+    }
+}

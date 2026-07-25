9ed53a94559ee63973d5aa852f2b14d4725653cd
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-25 05:00:31 +0300

    Добавлен общий контракт для анализирования полученных диагностических данных

---
+++ Diagnostish.Domain/Interfaces/IAnalyzeDiagnosticInfo.cs
@@ -0,0 +1,8 @@
+﻿using Diagnostish.Domain.Common;
+
+namespace Diagnostish.Domain.Interfaces;
+
+public interface IAnalyzeDiagnosticInfo<TRawInfo, TInfo>
+{
+    ProvideResult<TInfo> AnalyzeInfo(ProvideResult<IReadOnlyList<TRawInfo>> provided);
+}
--- Diagnostish.Domain/Interfaces/IAnalyzeHardwareInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Diagnostish.Domain.Interfaces
-{
-    public interface IAnalyzeHardwareInfo<TInfo, TReport>
-    {
-        void AnalyzeInfo(IEnumerable<TInfo> ifno, TReport report);
-    }
-}

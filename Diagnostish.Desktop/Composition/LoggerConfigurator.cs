a2fe092d7d9fb2dd31215fa7aee9fcc851890ba0
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-25 05:26:10 +0300

    Конфигурация логгера

---
+++ Diagnostish.Desktop/Composition/LoggerConfigurator.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+
+namespace Diagnostish.Desktop.Composition;
+
+public static class LoggerConfigurator
+{
+    public static ILogger Create()
+    {
+        return new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File("logs/diagnostish-.txt",
+                          rollingInterval: RollingInterval.Day,
+                          outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}{NewLine}")
+        .CreateLogger();
+    }
+}

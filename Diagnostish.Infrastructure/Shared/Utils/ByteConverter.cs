d8412726977819c5b2ab4f6cb8b11da19bfa0080
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-25 05:14:45 +0300

    Класс для конвертации в ГБ

---
+++ Diagnostish.Infrastructure/Shared/Utils/ByteConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace Diagnostish.Infrastructure.Shared.Utils;
+
+public static class ByteConverter
+{
+    private const double BytesInGigabyte = 1024d * 1024 * 1024;
+
+    public static double ToGigabytes(ulong bytes, int decimals = 2) => Math.Round(bytes / BytesInGigabyte, decimals);
+
+    public static double ToGigabytes(double bytes, int decimals = 2) => Math.Round(bytes / BytesInGigabyte, decimals);
+}

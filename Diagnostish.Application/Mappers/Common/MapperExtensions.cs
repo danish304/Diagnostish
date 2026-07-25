3e2575be537d0a2e2c610419ad9b10751977c51e
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-25 05:21:30 +0300

    Класс расширений для мапперов данных

---
+++ Diagnostish.Application/Mappers/Common/MapperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Diagnostish.Domain.Common;
+using Diagnostish.Domain.Models.Reports;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Diagnostish.Application.Mappers.Common;
+
+public static class MapperExtensions
+{
+    public static bool TryExtractData<TData>(this IssuesReport report,
+                                             ProvideResult<TData> analysisData,
+                                             [NotNullWhen(true)] out TData? data) where TData : class
+    {
+        report.Warnings.AddRange(analysisData.Warnings);
+        report.CriticalErrors.AddRange(analysisData.CriticalErrors);
+
+        data = analysisData.Data;
+        return data != null;
+    }
+}

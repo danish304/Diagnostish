eccdfcd7345144ab5283cc65ebb624cb4c5f253b
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-25 05:32:54 +0300

    Точка входа в приложение, все регистрации, логи

--- Diagnostish.Desktop/Program.cs
+++ Diagnostish.Desktop/Program.cs
@@ -1,17 +1,23 @@
-﻿using Diagnostish.Application.Services;
+﻿using Diagnostish.Application.Mappers.HardwareMappers;
+using Diagnostish.Application.Mappers.OperatingSystemMappers;
+using Diagnostish.Application.Services;
+using Diagnostish.Desktop.Composition;
 using Diagnostish.Desktop.Controllers;
 using Diagnostish.Desktop.Views;
+using Diagnostish.Desktop.Views.Common;
 using Diagnostish.Desktop.Views.HardwareInfoPrinters;
-using Diagnostish.Desktop.Views.Helpers;
 using Diagnostish.Desktop.Views.OperatingSystemInfoPrinters;
 using Diagnostish.Desktop.Views.UserInterfaces;
 using Diagnostish.Domain.Interfaces;
 using Diagnostish.Domain.Models.Entities;
+using Diagnostish.Domain.Models.Entities.Hardware;
 using Diagnostish.Domain.Models.Reports;
-using Diagnostish.Infrastructure.HardwareInfoAnalyzers;
-using Diagnostish.Infrastructure.HardwareInfoProviders;
-using Diagnostish.Infrastructure.OperatingSystemInfoProviders;
-using Diagnostish.Infrastructure.WmiHelpers;
+using Diagnostish.Infrastructure.Analyzers.HardwareInfoAnalyzers;
+using Diagnostish.Infrastructure.Analyzers.OperatingSystemInfoAnalyzers;
+using Diagnostish.Infrastructure.Providers.HardwareInfoProviders;
+using Diagnostish.Infrastructure.Providers.HardwareInfoProviders.RawHardwareInfo;
+using Diagnostish.Infrastructure.Providers.OperatingSystemInfoProviders;
+using Diagnostish.Infrastructure.Shared.Wmi.Executor;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
@@ -20,39 +26,46 @@ static class Program
     static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()  
-            .WriteTo.File("logs/diagnostish-.txt", 
-                          rollingInterval: RollingInterval.Day,
-                          outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}------------------------------------{NewLine}") 
-            .CreateLogger();
+        Log.Logger = LoggerConfigurator.Create();
 
         try
         {
             Log.Information("Приложение Diagnostish запущено.");
 
-            var services = new ServiceCollection();
+            var services = new ServiceCollection()
+                .AddSingleton(Log.Logger)
 
-            services.AddSingleton<IExecutorWmi, ExecutorWmi>();
+                .AddSingleton<IExecutorWmi, ExecutorWmi>()
 
-            services.AddSingleton<IProvideHardwareInfo, BaseboardInfoWmiProvider>();
-            services.AddSingleton<IProvideHardwareInfo, BiosInfoWmiProvider>();
-            services.AddSingleton<IProvideHardwareInfo, CpuInfoWmiProvider>();
-            services.AddSingleton<IProvideHardwareInfo, DrivesInfoWmiProvider>();
-            services.AddSingleton<IProvideHardwareInfo, GpuInfoWmiProvider>();
-            services.AddSingleton<IProvideHardwareInfo, RamInfoWmiProvider>();
-            services.AddSingleton<IProvideOperatingSystemInfo, OperatingSystemInfoWmiProvider>();
+                .AddComponent<HardwareReport, 
+                              RawBaseBoardInfo, BaseBoardInfo, 
+                              BaseBoardInfoWmiProvider, BaseBoardInfoAnalyzer, BaseBoardReportMapper>()
+                .AddComponent<HardwareReport,
+                              RawBiosInfo, BiosInfo, 
+                              BiosInfoWmiProvider, BiosInfoAnalyzer, BiosReportMapper>()
+                .AddComponent<HardwareReport, 
+                              RawCpuInfo, CpuInfo, 
+                              CpuInfoWmiProvider, CpuInfoAnalyzer, CpuReportMapper>()
+                .AddComponent<HardwareReport, 
+                              RawGpuInfo, IReadOnlyList<GpuInfo>, 
+                              GpuInfoWmiProvider, GpuInfoAnalyzer, GpuReportMapper>()
+                .AddComponent<HardwareReport, 
+                              RawRamInfo, RamInfo, 
+                              RamInfoWmiProvider, RamInfoAnalyzer, RamReportMapper>()
+                .AddComponent<HardwareReport, 
+                              RawStorageDriveInfo, IReadOnlyList<StorageDriveInfo>, 
+                              StorageDriveInfoWmiProvider, StorageDriveInfoAnalyzer, StorageDriveReportMapper>()
+                .AddComponent<OperatingSystemReport, 
+                              RawOperatingSystemInfo, OperatingSystemInfo, 
+                              OperatingSystemInfoWmiProvider, OperatingSystemInfoAnalyzer, OperatingSystemReportMapper>()
 
-            services.AddSingleton<IAnalyzeHardwareInfo<RamInfo, HardwareReport>, RamInfoAnalyzer>();
+                .AddPrinter<HardwareReport, HardwareInfoPrintToConsole>()
+                .AddPrinter<OperatingSystemReport, OperatingSystemInfoPrintToConsole>()
 
-            services.AddSingleton<ServicesAggregator>();
-
-            services.AddSingleton<IReportPrinter<HardwareReport>, HardwareInfoPrintToConsole>();
-            services.AddSingleton<IReportPrinter<OperatingSystemReport>, OperatingSystemInfoPrintToConsole>();
-            services.AddSingleton<IUserInterface, ConsoleUserInterface>();
-
-            services.AddSingleton<DiagnosticController>();
+                .AddSingleton<IUserInterface, ConsoleUserInterface>()
+                .AddSingleton<ServicesAggregator>()
+                .AddSingleton<PrintersAggregator>()
+                .AddSingleton<DiagnosticController>();
 
             var serviceProvider = services.BuildServiceProvider();
 

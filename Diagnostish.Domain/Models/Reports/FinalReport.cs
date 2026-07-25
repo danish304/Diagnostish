27c43265ba3c130f065aa66b18ceb4b438b927e4
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-25 05:05:21 +0300

    Финальный связующий  отчет, в дальнейшем формируется в агрегаторе

--- Diagnostish.Domain/Models/Reports/FinalReport.cs
+++ Diagnostish.Domain/Models/Reports/FinalReport.cs
@@ -1,8 +1,7 @@
-﻿namespace Diagnostish.Domain.Models.Reports
+﻿namespace Diagnostish.Domain.Models.Reports;
+
+public class FinalReport
 {
-    public class FinalReport
-    {
-        public HardwareReport HardwareReport { get; init; } = new();
-        public OperatingSystemReport OperatingSystemReport { get; init; } = new();
-    }
+    public HardwareReport HardwareReport { get; init; } = new();
+    public OperatingSystemReport OperatingSystemReport { get; init; } = new();
 }

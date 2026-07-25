624e16b314cb8fe096cf5b1d784eb52f581f6c70
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-25 05:30:29 +0300

    Агрегатор принтеров (агрегирует перечисления различного рода принтеров, вызывает для данных финального отчета соответствующие принтеры)

---
+++ Diagnostish.Desktop/Views/PrintersAggregator.cs
@@ -0,0 +1,25 @@
+﻿using Diagnostish.Domain.Interfaces;
+using Diagnostish.Domain.Models.Reports;
+
+namespace Diagnostish.Desktop.Views;
+
+public class PrintersAggregator
+{
+    private readonly IEnumerable<IReportPrinter<HardwareReport>> _hardwarePrinters;
+    private readonly IEnumerable<IReportPrinter<OperatingSystemReport>> _operatingSystemPrinters;
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>")]
+    public PrintersAggregator(IEnumerable<IReportPrinter<HardwareReport>> hardwarePrinters,
+                              IEnumerable<IReportPrinter<OperatingSystemReport>> operatingSystemPrinters)
+    {
+        _hardwarePrinters = hardwarePrinters;
+        _operatingSystemPrinters = operatingSystemPrinters;
+    }
+
+    public void PrintAllReports(FinalReport finalReport)
+    {
+        foreach (var printer in _hardwarePrinters) printer.Print(finalReport.HardwareReport);
+
+        foreach (var printer in _operatingSystemPrinters) printer.Print(finalReport.OperatingSystemReport);
+    }
+}

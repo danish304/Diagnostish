680bc06e047f52ead6e2e4d67c9954203f4347cc
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-25 05:09:43 +0300

    Закрытая запись для передачи данных, предупреждений и ошибок

---
+++ Diagnostish.Domain/Common/ProvideResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Diagnostish.Domain.Common;
+
+public sealed record ProvideResult<TInfo>(TInfo? Data, IReadOnlyList<string> Warnings, IReadOnlyList<string> CriticalErrors)
+{
+    public static ProvideResult<TInfo> Ok(TInfo data, IReadOnlyList<string>? warnings = null)  
+        => new(data, warnings ?? [], []);
+
+    public static ProvideResult<TInfo> Fail(IReadOnlyList<string> warnings, IReadOnlyList<string> criticalErrors)  
+        => new(default, warnings, criticalErrors);
+}

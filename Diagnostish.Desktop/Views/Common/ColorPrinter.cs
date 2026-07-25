cfbbc55bf9de43920cce466b88d4b56b3bc49c39
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-07-25 05:27:10 +0300

    Хелперы для формата данных и цвета строк

---
+++ Diagnostish.Desktop/Views/Common/ColorPrinter.cs
@@ -0,0 +1,11 @@
+﻿namespace Diagnostish.Desktop.Views.Common;
+
+public static class ColorPrinter
+{
+    public static void WriteLineColored(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(text);
+        Console.ResetColor();
+    }
+}
---
+++ Diagnostish.Desktop/Views/Common/FormattingData.cs
@@ -0,0 +1,6 @@
+﻿namespace Diagnostish.Desktop.Views.Common;
+
+public static class FormattingData
+{
+    public static string FormatDate(DateTime date) => date == DateTime.MinValue ? "Unknown" : date.ToString("dd.MM.yyyy");
+}

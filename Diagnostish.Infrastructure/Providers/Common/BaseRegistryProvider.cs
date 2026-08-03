e3369093ff280046324dbbe5a1cf1a37fc9a9b7f
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-08-03 07:57:59 +0300

    Добавлен базовый класс поставки данных из реестра

---
+++ Diagnostish.Infrastructure/Providers/Common/BaseRegistryProvider.cs
@@ -0,0 +1,42 @@
+﻿using Diagnostish.Domain.Common;
+using Diagnostish.Domain.Interfaces;
+using Diagnostish.Infrastructure.Shared.Registry.Executor;
+using Microsoft.Win32;
+
+namespace Diagnostish.Infrastructure.Providers.Common;
+
+public abstract class BaseRegistryProvider<TRawInfo>(IExucutorRegistry executor) : IProvideDiagnosticInfo<TRawInfo>
+{
+    public async Task<ProvideResult<IReadOnlyList<TRawInfo>>> ProvideInfoAsync(CancellationToken cancellationToken = default)
+    {
+        var rawInfo = new List<TRawInfo>();
+        var warnings = new List<string>();
+        var criticalErrors = new List<string>();
+
+        await executor.ExecuteSafeReadAsync(RootPath, ContextName, warnings, criticalErrors, rootKey =>
+        {
+            foreach (var subKeyName in rootKey.GetSubKeyNames())
+            {
+                if (!IsRelevantSubKey(subKeyName)) continue;
+
+                using var subKey = rootKey.OpenSubKey(subKeyName);
+                if (subKey is null) continue;
+
+                var mappedItem = Map(subKey);
+                if (mappedItem is not null) rawInfo.Add(mappedItem);
+            }
+        }, cancellationToken);
+
+        return rawInfo.Count > 0
+            ? ProvideResult<IReadOnlyList<TRawInfo>>.Ok(rawInfo, warnings)
+            : ProvideResult<IReadOnlyList<TRawInfo>>.Fail(warnings, criticalErrors);
+    }
+
+    protected abstract string RootPath { get; }
+
+    protected abstract string ContextName { get; }
+
+    protected virtual bool IsRelevantSubKey(string subKeyName) => true;
+
+    protected abstract TRawInfo? Map(RegistryKey subKey);
+}

81978bbdfc668deac33d35008d7ed8364da23068
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-08-06 07:58:04 +0300

    Добавление элементов в коллекцию сервиса перенесено в методы расширения

--- Diagnostish.Desktop/Composition/ServiceCollectionExtensions.cs
+++ Diagnostish.Desktop/Composition/ServiceCollectionExtensions.cs
@@ -1,12 +1,77 @@
-﻿using Diagnostish.Application.Pipelines;
+﻿using Diagnostish.Application.Mappers.HardwareMappers;
+using Diagnostish.Application.Mappers.OperatingSystemMappers;
+using Diagnostish.Application.Pipelines;
+using Diagnostish.Application.Services;
+using Diagnostish.Desktop.Controllers;
+using Diagnostish.Desktop.Views;
+using Diagnostish.Desktop.Views.HardwareInfoPrinters;
+using Diagnostish.Desktop.Views.OperatingSystemInfoPrinters;
+using Diagnostish.Desktop.Views.UserInterfaces;
 using Diagnostish.Domain.Interfaces;
+using Diagnostish.Domain.Models.Entities;
+using Diagnostish.Domain.Models.Entities.Hardware;
+using Diagnostish.Domain.Models.Reports;
+using Diagnostish.Infrastructure.Analyzers.HardwareInfoAnalyzers;
+using Diagnostish.Infrastructure.Analyzers.OperatingSystemInfoAnalyzers;
+using Diagnostish.Infrastructure.Providers.HardwareInfoProviders;
+using Diagnostish.Infrastructure.Providers.HardwareInfoProviders.RawHardwareInfo;
+using Diagnostish.Infrastructure.Providers.HardwareInfoProviders.Registry;
+using Diagnostish.Infrastructure.Providers.HardwareInfoProviders.Wmi;
+using Diagnostish.Infrastructure.Providers.OperatingSystemInfoProviders;
+using Diagnostish.Infrastructure.Shared.Registry.Executor;
+using Diagnostish.Infrastructure.Shared.Wmi.Executor;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Diagnostish.Desktop.Composition;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddComponent<TReport, TRaw, TInfo,
+    public static IServiceCollection AddCoreServices(this IServiceCollection services)
+    {
+        return services
+            .AddSingleton<IExecutorWmi, ExecutorWmi>()
+            .AddSingleton<IExecutorRegistry, ExecutorRegistry>()
+            .AddSingleton<IUserInterface, ConsoleUserInterface>()
+            .AddSingleton<ServicesAggregator>()
+            .AddSingleton<PrintersAggregator>()
+            .AddSingleton<DiagnosticController>();
+    }
+
+    public static IServiceCollection AddHardwareComponents(this IServiceCollection services)
+    {
+        services.AddSingleton<GpuInfoWmiProvider>();
+        services.AddSingleton<GpuInfoRegistryProvider>();
+
+        return services
+            .AddComponent<HardwareReport, RawCpuInfo, CpuInfo, 
+                          CpuInfoWmiProvider, CpuInfoAnalyzer, CpuReportMapper>()
+            .AddComponent<HardwareReport, RawRamInfo, RamInfo, 
+                          RamInfoWmiProvider, RamInfoAnalyzer, RamReportMapper>()
+            .AddComponent<HardwareReport, RawGpuInfo, IReadOnlyList<GpuInfo>, 
+                          GpuInfoProvider, GpuInfoAnalyzer, GpuReportMapper>()
+            .AddComponent<HardwareReport, RawStorageDriveInfo, IReadOnlyList<StorageDriveInfo>, 
+                          StorageDriveInfoWmiProvider, StorageDriveInfoAnalyzer, StorageDriveReportMapper>()
+            .AddComponent<HardwareReport, RawBiosInfo, BiosInfo, 
+                          BiosInfoWmiProvider, BiosInfoAnalyzer, BiosReportMapper>()
+            .AddComponent<HardwareReport, RawBaseBoardInfo, BaseBoardInfo, 
+                          BaseBoardInfoWmiProvider, BaseBoardInfoAnalyzer, BaseBoardReportMapper>();
+    }
+
+    public static IServiceCollection AddOperatingSystemComponents(this IServiceCollection services)
+    {
+        return services
+            .AddComponent<OperatingSystemReport, RawOperatingSystemInfo, OperatingSystemInfo,
+                                     OperatingSystemInfoWmiProvider, OperatingSystemInfoAnalyzer, OperatingSystemReportMapper>();
+    }
+
+    public static IServiceCollection AddPrinters(this IServiceCollection services)
+    {
+        return services
+            .AddPrinter<HardwareReport, HardwareInfoPrintToConsole>()
+            .AddPrinter<OperatingSystemReport, OperatingSystemInfoPrintToConsole>();
+    }
+
+    private static IServiceCollection AddComponent<TReport, TRaw, TInfo,
                                                   TProvider, TAnalyzer, TMapper>(this IServiceCollection services)
 
                                                   where TProvider : class, IProvideDiagnosticInfo<TRaw>
@@ -32,7 +97,7 @@ public static class ServiceCollectionExtensions
         return services;
     }
 
-    public static IServiceCollection AddPrinter<TReport, TPrinter>(this IServiceCollection services) 
+    private static IServiceCollection AddPrinter<TReport, TPrinter>(this IServiceCollection services) 
         
                                                 where TPrinter : class, IReportPrinter<TReport>
     {
--- Diagnostish.Desktop/Program.cs
+++ Diagnostish.Desktop/Program.cs
@@ -1,24 +1,7 @@
-﻿using Diagnostish.Application.Mappers.HardwareMappers;
-using Diagnostish.Application.Mappers.OperatingSystemMappers;
-using Diagnostish.Application.Services;
-using Diagnostish.Desktop.Composition;
+﻿using Diagnostish.Desktop.Composition;
 using Diagnostish.Desktop.Controllers;
-using Diagnostish.Desktop.Views;
 using Diagnostish.Desktop.Views.Common;
-using Diagnostish.Desktop.Views.HardwareInfoPrinters;
-using Diagnostish.Desktop.Views.OperatingSystemInfoPrinters;
-using Diagnostish.Desktop.Views.UserInterfaces;
-using Diagnostish.Domain.Interfaces;
-using Diagnostish.Domain.Models.Entities;
-using Diagnostish.Domain.Models.Entities.Hardware;
-using Diagnostish.Domain.Models.Reports;
-using Diagnostish.Infrastructure.Analyzers.HardwareInfoAnalyzers;
-using Diagnostish.Infrastructure.Analyzers.OperatingSystemInfoAnalyzers;
-using Diagnostish.Infrastructure.Providers.HardwareInfoProviders.RawHardwareInfo;
-using Diagnostish.Infrastructure.Providers.HardwareInfoProviders.Wmi;
-using Diagnostish.Infrastructure.Providers.OperatingSystemInfoProviders;
 using Diagnostish.Infrastructure.Shared.Wmi;
-using Diagnostish.Infrastructure.Shared.Wmi.Executor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
@@ -44,44 +27,19 @@ static class Program
         {
             Log.Information("Приложение Diagnostish запущено.");
             Log.Information("Таймаут WMI-запросов выставлен на {Timeout} сек.", 
-                             configuration.GetValue("Wmi:WmiQueryTimeoutSeconds", new WmiSettings().WmiQueryTimeoutSeconds));
+                             configuration.GetValue("Wmi:WmiQueryTimeoutSeconds", 
+                                                     new WmiSettings().WmiQueryTimeoutSeconds));
 
-            var services = new ServiceCollection()
+            var serviceProvider = new ServiceCollection()
                 .AddSingleton(Log.Logger)
                 .AddSingleton(configuration).Configure<WmiSettings>(configuration.GetSection("Wmi"))
+                .AddCoreServices()
+                .AddHardwareComponents()
+                .AddOperatingSystemComponents()
+                .AddPrinters()
+                .BuildServiceProvider();
 
-                .AddSingleton<IExecutorWmi, ExecutorWmi>()
-
-                .AddComponent<HardwareReport, RawBaseBoardInfo, BaseBoardInfo, 
-                              BaseBoardInfoWmiProvider, BaseBoardInfoAnalyzer, BaseBoardReportMapper>()
-                .AddComponent<HardwareReport, RawBiosInfo, BiosInfo, 
-                              BiosInfoWmiProvider, BiosInfoAnalyzer, BiosReportMapper>()
-                .AddComponent<HardwareReport, RawCpuInfo, CpuInfo, 
-                              CpuInfoWmiProvider, CpuInfoAnalyzer, CpuReportMapper>()
-                .AddComponent<HardwareReport, RawGpuInfo, IReadOnlyList<GpuInfo>, 
-                              GpuInfoWmiProvider, GpuInfoAnalyzer, GpuReportMapper>()
-                .AddComponent<HardwareReport, RawRamInfo, RamInfo, 
-                              RamInfoWmiProvider, RamInfoAnalyzer, RamReportMapper>()
-                .AddComponent<HardwareReport, RawStorageDriveInfo, IReadOnlyList<StorageDriveInfo>, 
-                              StorageDriveInfoWmiProvider, StorageDriveInfoAnalyzer, StorageDriveReportMapper>()
-                .AddComponent<OperatingSystemReport, RawOperatingSystemInfo, OperatingSystemInfo, 
-                              OperatingSystemInfoWmiProvider, OperatingSystemInfoAnalyzer, OperatingSystemReportMapper>()
-
-                .AddPrinter<HardwareReport, HardwareInfoPrintToConsole>()
-                .AddPrinter<OperatingSystemReport, OperatingSystemInfoPrintToConsole>()
-
-                .AddSingleton<IUserInterface, ConsoleUserInterface>()
-
-                .AddSingleton<ServicesAggregator>()
-                .AddSingleton<PrintersAggregator>()
-
-                .AddSingleton<DiagnosticController>();
-
-            var serviceProvider = services.BuildServiceProvider();
-
-            var controller = serviceProvider.GetRequiredService<DiagnosticController>();
-
-            await controller.StartDiagnosticAsync(cts.Token);
+            await serviceProvider.GetRequiredService<DiagnosticController>().StartDiagnosticAsync(cts.Token);
 
             Log.Information("Приложение Diagnostish завершило свою работу.");
         }

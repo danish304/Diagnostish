3f04c122fcd6efef5e48b05d9d6b3018583e5206
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-08-14 13:20:00 +0300

    Общие сообщения вынесены на уровень выше

---
+++ Diagnostish.Infrastructure/Analyzers/Common/CommonMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Diagnostish.Infrastructure.Analyzers.Common;
+
+internal static class CommonMessages
+{
+    internal static string CountOfTotal(int count, int total)
+    {
+        return $"({count} из {total})";
+    }
+}

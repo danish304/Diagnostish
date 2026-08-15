0147d1401e55fe51f58680ae81b2701ab42a23b4
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-08-15 10:41:04 +0300

    Исправлены ошибки в названиях

--- Infrastructure/Providers/Wmi/Network/NetworkAdapterWmiProvider.cs
+++ Infrastructure/Providers/Wmi/Network/NetworkAdapterWmiProvider.cs
@@ -6,15 +6,15 @@ using System.Management;
 
 namespace Infrastructure.Providers.Wmi.Network;
 
-public class NetworkAdatperWmiProvider(IWmiExecutor executor)
+public class NetworkAdapterWmiProvider(IWmiExecutor executor)
     : BaseWmiProvider<NetworkAdapterRawModel>(executor)
 {
-    private const string DESCRIPRION = "Description";
+    private const string DESCRIPTION = "Description";
     private const string MAC_ADDRESS = "MACAddress";
     private const string DHCP_ENABLED = "DHCPEnabled";
 
     protected override string BuildQuery() =>
-        $"SELECT {DESCRIPRION}, {MAC_ADDRESS}, {DHCP_ENABLED} " +
+        $"SELECT {DESCRIPTION}, {MAC_ADDRESS}, {DHCP_ENABLED} " +
         $"FROM Win32_NetworkAdapterConfiguration " +
         $"WHERE IPEnabled = True";
 
@@ -23,7 +23,7 @@ public class NetworkAdatperWmiProvider(IWmiExecutor executor)
     protected override NetworkAdapterRawModel Map(ManagementBaseObject item)
     {
         return new NetworkAdapterRawModel(
-            Parser.ToSafeString(item[DESCRIPRION]),
+            Parser.ToSafeString(item[DESCRIPTION]),
             Parser.ToSafeString(item[MAC_ADDRESS]),
             Parser.ToSafeString(item[DHCP_ENABLED])
         );

3caa06f6bd0260556436ab0ef3497485d523862f
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-08-15 11:18:49 +0300

    Добавлена регистрация интерфейсов

--- Desktop/Program.cs
+++ Desktop/Program.cs
@@ -38,7 +38,8 @@ static class Program
                 .AddHardwareComponents()
                 .AddOperatingSystemComponents()
                 .AddNetworkComponents()
-                .AddPrinters();
+                .AddPrinters()
+                .AddUserInterfaces();
 
             using var serviceProvider = services.BuildServiceProvider();
 
@@ -49,9 +50,7 @@ static class Program
         }
         catch (Exception ex)
         {
-            ColorPrinter.WriteLineColored(
-                "\nПРОИЗОШЛА КРИТИЧЕСКАЯ ОШИБКА!",
-                ConsoleColor.Red);
+            ColorPrinter.WriteLineColored("\nПРОИЗОШЛА КРИТИЧЕСКАЯ ОШИБКА!", ConsoleColor.Red);
 
             Log.Fatal(ex, "Необработанное исключение. Приложение аварийно завершило свою работу.");
         }

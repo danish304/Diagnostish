75e7fffb9ccef64191b126dc551bf7e1b3d875b5
Даниил Цветков <dankatsvetkov10@gmail.com>
2026-08-22 01:04:02 +0300

    Диспетчеры теперь в отдельной папке

---
+++ Desktop/Views/Dispatchers/FinalReportPrintDispatcher.cs
@@ -0,0 +1,36 @@
+﻿namespace Desktop.Views.Dispatchers;
+
+public class FinalReportPrintDispatcher
+{
+    private readonly IEnumerable<IReportPrinter<HardwareReport>> _hardwarePrinters;
+    private readonly IEnumerable<IReportPrinter<OperatingSystemReport>> _operatingSystemPrinters;
+    private readonly IEnumerable<IReportPrinter<NetworkReport>> _networkPrinters;
+
+    public FinalReportPrintDispatcher(
+        IEnumerable<IReportPrinter<HardwareReport>> hardwarePrinters,
+        IEnumerable<IReportPrinter<OperatingSystemReport>> operatingSystemPrinters,
+        IEnumerable<IReportPrinter<NetworkReport>> networkPrinters)
+    {
+        _hardwarePrinters = hardwarePrinters;
+        _operatingSystemPrinters = operatingSystemPrinters;
+        _networkPrinters = networkPrinters;
+    }
+
+    public void PrintAllReports(FinalReport report)
+    {
+        foreach (var printer in _hardwarePrinters)
+        {
+            printer.Print(report.HardwareReport);
+        }
+
+        foreach (var printer in _operatingSystemPrinters)
+        {
+            printer.Print(report.OperatingSystemReport);
+        }
+
+        foreach (var printer in _networkPrinters)
+        {
+            printer.Print(report.NetworkReport);
+        }
+    }
+}
---
+++ Desktop/Views/Dispatchers/UserInterfaceDispatcher.cs
@@ -0,0 +1,27 @@
+﻿namespace Desktop.Views.Dispatchers;
+
+public class UserInterfaceDispatcher
+{
+    private readonly IEnumerable<IUserInterface> _interfaces;
+
+    public UserInterfaceDispatcher(IEnumerable<IUserInterface> interfaces)
+    {
+        _interfaces = interfaces;
+    }
+
+    public void ShowWelcomes()
+    {
+        foreach (var ui in _interfaces)
+        {
+            ui.ShowWelcome();
+        }
+    }
+
+    public void ShowCompletions()
+    {
+        foreach (var ui in _interfaces)
+        {
+            ui.ShowCompletion();
+        }
+    }
+}
